7709574f3782487c66eaf273a73b7218dc502390
Hritick <hritickbhushan@gmail.com>
2022-08-04 10:36:21 +0530

    Fixed some problems

--- Expenses-chart-component app/nextapp/.next/static/chunks/webpack.js
+++ Expenses-chart-component app/nextapp/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "de74602e224a526b"; }
+/******/ 		__webpack_require__.h = function() { return "83a415fb9f7c2c05"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
